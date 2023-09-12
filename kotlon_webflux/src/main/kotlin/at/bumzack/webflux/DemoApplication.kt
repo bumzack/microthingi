14450ea6cd0528132f1215d66e1a699bc5dbac85
bumzacktrara <bumzacktrara@gmail.com>
2023-09-13 00:16:22 +0200

    spring webflux kotlin

---
+++ kotlon_webflux/src/main/kotlin/at/bumzack/webflux/DemoApplication.kt
@@ -0,0 +1,11 @@
+package at.bumzack.webflux
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
